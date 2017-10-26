3fece1aee2026efd257563bbc08a461943b307bd
martin <vi.fri.martin@gmail.com>
2017-10-26 15:04:38 +0200

    savepoint

--- ex2/COOtoCRS.cpp
+++ ex2/COOtoCRS.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
+#include <vector>
 
 using namespace Eigen;
 
@@ -14,7 +15,34 @@ class MatrixCOO {
 	// you can work directly with the raw vectors;
 	// (recommended) you can implement functions toCRS and toDense for your wrappers;
 	// you can overload operator= for implicit/explicit conversion.
+    //
+    public:
+        MatrixCOO toCOO(MatrixXd &A);
+    private:
+        std::vector<Eigen::Triplet<double>> TripletList;
 };
+
+
+MatrixCOO::MatrixCOO(std::vector<Eigen::Triplet<double>> T){
+    TripletList = T;
+};
+
+MatrixCOO MatrixCOO::toCOO(MatrixXd &A){
+    size_t num_cols = A.cols();
+    size_t num_rows = A.rows();
+    std::vector<Eigen::triplet<double>> Tlist;
+
+    for(size_t n=0;n<num_rows;n++){
+        for(size_t m=0;m<num_cols;m++){
+            if(A(m,n) != 0){
+                Tlist.push(Triplet(m, n, A(m,n)));
+            }
+        }
+    }
+    return MatrixCOO(Tlist);
+};
+
+
 
 class MatrixCRS {
 	// TODO: implement CRS data structures, conversion to COO and to/from dense Eigen matrix.
